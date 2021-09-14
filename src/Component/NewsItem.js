9080acc06a0c4782588ade07d8e70edf3d24a1de
Saurav singh <sauravsingh85079@gmail.com>
2021-09-15 02:12:40 +0530

    Add News structure and items

--- src/Component/NewsItem.js
+++ src/Component/NewsItem.js
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
+
 
 export default class NewsItem extends Component {
-  static propTypes = {
-    prop: PropTypes,
-  };
-
+  
+  
   render() {
     let { title, description , imageUrl} = this.props;
     return (
+      
       <div className="my-3">
         <div className="card" style={{width: "18rem"}}>
           <img src={imageUrl} className="card-img-top" alt="..." />
@@ -16,11 +15,12 @@ export default class NewsItem extends Component {
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
             <a href="#" className="btn btn-primary">
-              Go somewhere
+              Read More 
             </a>
           </div>
         </div>
       </div>
     );
   }
+  
 }
